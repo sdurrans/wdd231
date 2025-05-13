71356b440112481e089a01d9c0c9d3fbab942ee9
sdurrans <sdurrant@byupathway.edu>
2025-05-13 00:47:40 -0400

    formating

--- familyNames.js
+++ familyNames.js
@@ -1,55 +1,30 @@
+
 {
-    "families": [
+    "familyName": "Durrant",
+    "movedInDate": "2024-12-15",
+    "numberOfPeople": 4,
+    "visitedByBishopric": true,
+    "members": [
         {
-            "familyName": "Durrant",
-            "movedInDate": "2024-12-15",
-            "numberOfPeople": 4,
-            "visitedByBishopric": true,
-            "members": [
-                {
-                    "name": "Stuart Durrant",
-                    "gender": "Male",
-                    "birthdate": "1987-10-07"
-                },
-                {
-                    "name": "Jessica Durrant",
-                    "gender": "Female",
-                    "birthdate": "1990-02-19"
-                },
-                {
-                    "name": "Brigham Durrant",
-                    "gender": "Male",
-                    "birthdate": "2011-09-03"
-                },
-                {
-                    "name": "Maverick Durrant",
-                    "gender": "Male",
-                    "birthdate": "2015-09-05"
-                }
-            ]
+            "name": "Stuart Durrant",
+            "gender": "Male",
+            "birthdate": "1987-10-07"
+        },  
+        {
+            "name": "Jessica Durrant",
+            "gender": "Female",
+            "birthdate": "1990-02-19"
         },
         {
-            "familyName": "Northrem",
-            "movedInDate": "2020-05-12",
-            "numberOfPeople": 3,
-            "visitedByBishopric": false,
-            "members": [
-                {
-                    "name": "Kenneth Northrem",
-                    "gender": "Male",
-                    "birthdate": "1974-10-06"
-                },
-                {
-                    "name": "Megan Northrem",
-                    "gender": "Female",
-                    "birthdate": "1976-04-19"
-                },
-                {
-                    "name": "Molly Northrem",
-                    "gender": "Female",
-                    "birthdate": "2004-08-17"
-                }
-            ]
+            "name": "Brigham Durrant",
+            "gender": "Male",
+            "birthdate": "2011-09-03"
+        },
+        {
+            "name": "Maverick Durrant",
+            "gender": "Male",
+            "birthdate": "2015-09-05"
         }
     ]
-  }
+}
+
