da1564e92e6bbfc046346f0cb2a8b5c6315261a7
sdurrans <sdurrant@byupathway.edu>
2025-05-13 00:44:15 -0400

    json family names

---
+++ familyNames.js
@@ -0,0 +1,55 @@
+{
+    "families": [
+        {
+            "familyName": "Durrant",
+            "movedInDate": "2024-12-15",
+            "numberOfPeople": 4,
+            "visitedByBishopric": true,
+            "members": [
+                {
+                    "name": "Stuart Durrant",
+                    "gender": "Male",
+                    "birthdate": "1987-10-07"
+                },
+                {
+                    "name": "Jessica Durrant",
+                    "gender": "Female",
+                    "birthdate": "1990-02-19"
+                },
+                {
+                    "name": "Brigham Durrant",
+                    "gender": "Male",
+                    "birthdate": "2011-09-03"
+                },
+                {
+                    "name": "Maverick Durrant",
+                    "gender": "Male",
+                    "birthdate": "2015-09-05"
+                }
+            ]
+        },
+        {
+            "familyName": "Northrem",
+            "movedInDate": "2020-05-12",
+            "numberOfPeople": 3,
+            "visitedByBishopric": false,
+            "members": [
+                {
+                    "name": "Kenneth Northrem",
+                    "gender": "Male",
+                    "birthdate": "1974-10-06"
+                },
+                {
+                    "name": "Megan Northrem",
+                    "gender": "Female",
+                    "birthdate": "1976-04-19"
+                },
+                {
+                    "name": "Molly Northrem",
+                    "gender": "Female",
+                    "birthdate": "2004-08-17"
+                }
+            ]
+        }
+    ]
+  }
